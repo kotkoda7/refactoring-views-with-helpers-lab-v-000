4efc75e63b7320d63ac43251008cc9d7ebc48f92
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-13 23:16:35 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,6 @@
 module ArtistsHelper
   def display_artist(song)
-    if song.artists
+    if song.artist
       link_to song.artist.name, artist_path(song.artist)
     else
       link_to "Add Artist", edit_song_path(song)
